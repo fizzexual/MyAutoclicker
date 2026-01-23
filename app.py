77db2f645595d8ff844dea3acf15175d27c826f7
Fizz <fizzexual@gmail.com>
2026-01-23 23:34:41 +0200

    Fixed bug where autoclicker opens 2 times

--- app.py
+++ app.py
@@ -5,9 +5,9 @@ import os
 app = Flask(__name__)
 clicker_script = os.path.join(os.path.dirname(os.path.abspath(__file__)), "fast_autoclicker.py")
 
-
-subprocess.Popen(["python", clicker_script])
-print("fast_autoclicker.py launched!")
+if __name__ == "__main__" and os.environ.get("WERKZEUG_RUN_MAIN") == "true":
+    subprocess.Popen(["python", clicker_script])
+    print("fast_autoclicker.py launched!")
 
 @app.route("/")
 def index():
